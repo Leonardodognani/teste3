3ed8cc3f791809d5dac7328282dae1e6d28784a6
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2022-02-01 11:04:05 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Olá, digite seu nome: ");
+string name = Console.ReadLine();
+
+Console.Write($"Muito obrigado, {name}. Agora, por favor, insira o ano de seu nascimento: ");
+int year = int.Parse(Console.ReadLine());
+int age = 2022 - year;
+
+    if(age > 110){
+        Console.WriteLine($"Wow! {name}, {age} anos! Praticamente um matusalém!");
+    }else if(age < 110 && age >= 100){
+        Console.WriteLine($"{name}, {age} anos!? Batendo o século hein?");
+    }else if(age < 100 && age >= 18){
+        Console.WriteLine($"{name}, você tem {age} anos, portanto, é maior de idade.");
+    }else if(age < 18 && age >= 0){
+        Console.WriteLine($"{name} possui apenas {age} anos, portanto, é menor de idade.");
+    }else if(age < 0){
+        Console.WriteLine($"Nossa!, {name}, ou você é um viajante do tempo ou errou esse número aí =)");
+    }
+
